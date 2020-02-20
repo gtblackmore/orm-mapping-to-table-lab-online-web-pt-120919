49a35d90cffdfa91dfdf44357ef3fab8ce48f5e4
George Blackmore <gtblackmore@gmail.com>
2020-02-20 03:49:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,13 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize (name, grade, id = nil)
+  def initialize (name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
   end
   
-  
+  def self.create_table
+    
   
 end
