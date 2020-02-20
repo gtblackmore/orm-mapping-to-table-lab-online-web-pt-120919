b5a01fd4e16c9b423738ce2ed9d0a7a8426a2948
George Blackmore <gtblackmore@gmail.com>
2020-02-20 03:54:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,5 +25,14 @@ class Student
   end
   
   def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
+  
+      
   
 end
