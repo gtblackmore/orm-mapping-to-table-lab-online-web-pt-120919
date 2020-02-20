493148ef2c5d62229b2894e527c16c6aab30c9a5
George Blackmore <gtblackmore@gmail.com>
2020-02-20 03:48:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
   attr_accessor :name, :grade
   
-  @@all = []
-  
   def initialize (name, grade, id = nil)
     @name = name
     @grade = grade
-    @@all << self
+  end
+  
+  
   
 end
