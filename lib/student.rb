1bf3776db964bcb023863f2ff3f29a9170d1d2f9
George Blackmore <gtblackmore@gmail.com>
2020-02-20 03:47:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  
+  @@all = []
+  
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @@all << self
   
 end
