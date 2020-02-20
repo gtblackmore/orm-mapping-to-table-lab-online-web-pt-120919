385c30f143bc55d296c9135a27d6be7426b6b9bb
George Blackmore <gtblackmore@gmail.com>
2020-02-20 03:49:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,13 @@ class Student
   end
   
   def self.create_table
-    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   
 end
